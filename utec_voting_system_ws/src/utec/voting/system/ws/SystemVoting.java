1bbd86370f283a346d509bc85b9ae0d4e67caac1
UNCKevinOrellana <kevin_orellana@unicomer.com>
2019-09-30 12:39:23 -0600

    Creación de proyecto para API Rest

---
+++ utec_voting_system_ws/src/utec/voting/system/ws/SystemVoting.java
@@ -0,0 +1,9 @@
+package utec.voting.system.ws;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/service")
+public class SystemVoting extends Application{
+
+}
