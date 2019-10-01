19c4f4bf5a94623007c20b09292949994d434f7b
kevinAguilar208415 <keeviinoreellana@gmail.com>
2019-09-30 23:06:43 -0600

    Creacion de de entities, servicios

--- utec_voting_system_ws/src/utec/voting/system/ws/UsuarioService.java
+++ utec_voting_system_ws/src/utec/voting/system/ws/UsuarioService.java
@@ -1,18 +1,61 @@
 package utec.voting.system.ws;
 
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+
+import utec.voting.system.entities.Usuario;
+import utec.voting.system.services.Service;
 
 @Path("/usuario")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-public class UsuarioService {
-	@GET
-	  public Response sayHello() {
-	    return Response.ok("Hello World desde el API REST",MediaType.APPLICATION_JSON).build();
-	  }
+public class UsuarioService implements Service<Usuario>, Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public ArrayList<Usuario> getAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean save(Usuario t) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean update(Usuario t) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean delete(Usuario t) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario finById(Integer id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario finById(String id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
