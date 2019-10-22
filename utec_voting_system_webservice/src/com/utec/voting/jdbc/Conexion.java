83eb510dd827c13e3be8e296a146fb8c714bea2a
kevinAguilar208415 <keeviinoreellana@gmail.com>
2019-10-21 22:49:52 -0600

    Cambios en general

--- utec_voting_system_webservice/src/com/utec/voting/jdbc/Conexion.java
+++ utec_voting_system_webservice/src/com/utec/voting/jdbc/Conexion.java
@@ -1,7 +1,6 @@
 package com.utec.voting.jdbc;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
--- utec_voting_system_webservice/src/utec/voting/system/ws/UsuarioService.java
+++ utec_voting_system_webservice/src/utec/voting/system/ws/UsuarioService.java
@@ -10,11 +10,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.utec.voting.jdbc.Conexion;
 
-import utec.voting.system.entities.Persona;
-import utec.voting.system.entities.TipoUsuario;
 import utec.voting.system.entities.Usuario;
 import utec.voting.system.services.Service;
 
@@ -62,10 +64,9 @@ public class UsuarioService extends Conexion implements Service<Usuario>, Serial
 	}
 
 	@Override
-	@GET
-	@Path("/findId/{id}")
 	public Usuario finById(@PathParam("id") String id) throws SQLException {
 		Usuario obj = null;
+		
 		try {
 			setPs(consPrepare(SELECT + TABLE + WHERE + "US_PER_DUI = ?"));
 			getPs().setString(1, id);
@@ -82,6 +83,46 @@ public class UsuarioService extends Conexion implements Service<Usuario>, Serial
 			getPs().close();
 		}
 		return obj;
+	}
+	
+	@SuppressWarnings("finally")
+	@GET
+	@Path("/findId/{id}")
+	public Response finByIds(@PathParam("id") String id) throws SQLException {
+		Usuario obj = null;
+		JSONObject jsonObject = null;
+		
+		try {
+			setPs(consPrepare(SELECT + TABLE + WHERE + "US_PER_DUI = ?"));
+			getPs().setString(1, id);
+			setRs(getPs().executeQuery());
+			if(getRs().next()) {
+				getRs().beforeFirst();
+				while (getRs().next()) {
+					System.out.println("Valor "+getRs().getString(1));
+				}	
+			}
+			jsonObject = new JSONObject(obj);
+		} catch (Exception e) {
+			return Response.status(200).entity(jsonObject).build();
+		} finally {
+			return Response.status(200).entity(jsonObject).build();
+		}
+	}
+	
+	@GET
+	@Path("")
+	public Response convertFtoC() throws JSONException {
+ 
+		JSONObject jsonObject = new JSONObject();
+		Double fahrenheit = 98.24;
+		Double celsius;
+		celsius = (fahrenheit - 32) * 5 / 9;
+		jsonObject.put("F Value", fahrenheit);
+		jsonObject.put("C Value", celsius);
+ 
+		String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n" + jsonObject;
+		return Response.status(200).entity(result).build();
 	}
 
 }
