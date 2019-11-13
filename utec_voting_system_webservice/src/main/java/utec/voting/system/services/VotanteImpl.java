cd5425a7b9bdf6146c96ad5688ba7079f4bc1985
Alcapone-Fx <navasfx19@gmail.com>
2019-11-13 00:47:28 -0600

    Implementacion completa de votante

--- utec_voting_system_webservice/src/main/java/utec/voting/system/services/VotanteImpl.java
+++ utec_voting_system_webservice/src/main/java/utec/voting/system/services/VotanteImpl.java
@@ -60,20 +60,51 @@ public class VotanteImpl extends Conexion implements Service<Votante>, Serializa
 	}
 
 	@Override
-	public Votante save(Votante t) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public Votante save(Votante votante) throws SQLException {
+		CallableStatement statement = null;
+		try {
+			String query = "{CALL SP_CREATE_VOTANTE(?,?,?,?)}";
+			statement = getConnection().prepareCall(query);
+			
+			statement.setString(1, votante.getVotPerDui().getPerDui());
+			statement.setDate(2, votante.getVotFechaVence());
+			statement.setDate(3, votante.getVotFechaExp());
+			statement.setInt(4, votante.getEstado());
+			
+			statement.execute();
+		} catch (Exception e) {
+			logger.error("Error: " + e.getMessage());
+		}finally {
+			statement.close();
+		}
+		return votante;
 	}
 
 	@Override
-	public Boolean update(Votante t) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public Boolean update(Votante votante) throws SQLException {
+		CallableStatement statement =  null;
+		
+		try {
+			String query = "{CALL SP_UPDATE_VOTANTE(?,?,?,?)}";
+			statement = getConnection().prepareCall(query);
+			
+			statement.setString(1, votante.getVotPerDui().getPerDui());
+			statement.setDate(2, votante.getVotFechaVence());
+			statement.setDate(3, votante.getVotFechaExp());
+			statement.setInt(4, votante.getEstado());
+			
+			statement.execute();
+		} catch (Exception e) {
+			logger.error("Error: " + e.getMessage());
+		}finally {
+			statement.close();
+		}
+		return true;
 	}
 
 	@Override
 	public Boolean delete(Votante t) throws SQLException {
-		// TODO Auto-generated method stub
+		// No se deberia eliminar a un votante
 		return null;
 	}
 
@@ -84,9 +115,37 @@ public class VotanteImpl extends Conexion implements Service<Votante>, Serializa
 	}
 
 	@Override
-	public Votante finById(String id) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public Votante finById(String dui) throws SQLException {
+		CallableStatement statement = null;
+		Persona persona = new Persona();
+		Votante votante = new Votante();
+		
+		try {
+			String query = "{CALL SP_READ_ONE_VOTANTE(?)}";
+			statement = getConnection().prepareCall(query);
+			statement.setString(1, dui);
+			setRs(statement.executeQuery());
+			
+			if (getRs().next()) {
+				getRs().beforeFirst();
+				while (getRs().next()) {
+					persona = new Persona();
+					
+					persona = personaService.finById(getRs().getString(1));
+					votante = new Votante();
+					
+					votante.setVotPerDui(persona);
+					votante.setVotFechaVence(getRs().getDate(2));
+					votante.setVotFechaExp(getRs().getDate(3));
+					votante.setEstado(getRs().getInt(4));					
+				}
+			}
+		} catch (Exception e) {
+			logger.error("Error: " + e.getMessage());
+		}finally {
+			statement.close();
+		}							
+		return votante;
 	}
 
 }
