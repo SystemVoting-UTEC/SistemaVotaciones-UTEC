e2a6e2dd684dc9ea7f546e7dc446e4fc4d424fba
meme24-1992 <manu.cardona92@gmail.com>
2019-11-13 21:56:25 -0600

    creacion de ws persona y sufragio

--- utec_voting_system_webservice/src/main/java/utec/voting/system/services/SufragioImpl.java
+++ utec_voting_system_webservice/src/main/java/utec/voting/system/services/SufragioImpl.java
@@ -38,21 +38,71 @@ public class SufragioImpl extends Conexion implements Service<Sufragio>, Seriali
 	
 	@Override
 	public ArrayList<Sufragio> getAll() throws SQLException {
-		
-		return null;
-		
+		Sufragio g;
+		Candidato candidato = null;
+		Persona persona = null;
+		ArrayList<Sufragio> l1 = new ArrayList<>();
+		CallableStatement stmt = null;
+		try {
+			String query = "{CALL SP_READ_SufragioS()}";
+			stmt = getConnection().prepareCall(query);
+			setRs(stmt.executeQuery());
+			if(getRs().next()) {
+				getRs().beforeFirst();
+				while (getRs().next()) {
+					persona = PersonaService.finById(getRs().getInt("SUF_PER_DUI"));
+					candidato = CandidatoService.finById(getRs().getInt("SUF_CAN_ID"));
+					g = new Sufragio(getRs().getInt(1),persona,candidato, getRs().getString(2),getRs().getInt(3),getRs().getdouble(4));
+					l1.add(g);
+				}
+			}
+		} catch (Exception e) {
+			logger.error("Error: " + e);
+		}finally {
+			stmt.close();			
+		}
+		return l1;
 	}
 	
 	@Override
 	public Sufragio save(Sufragio t) throws SQLException {
-		
-		return null;
+		CallableStatement stmt = null;
+		try {
+			String query = "{CALL SP_CREATE_Sufragio(?,?)}";
+			stmt = getConnection().prepareCall(query);
+			stmt.setString(1, t.getGenSufragio());
+			stmt.registerOutParameter(2, Types.INTEGER);
+			stmt.execute();
+			if (stmt.getInt(2) > 0) {
+				t.setGenId(stmt.getInt(2));
+			}
+		} catch (Exception e) {
+			logger.error("Error" + e);
+		}finally {
+			stmt.close();			
+		}
+		return t;
 	}
 
 	@Override
 	public Boolean update(Sufragio t) throws SQLException {
-		
-		return null;
+		CallableStatement stmt = null;
+		try {
+			String query = "{CALL SP_UPDATE_Sufragio(?,?,?)}";
+			stmt = getConnection().prepareCall(query);
+			stmt.setString(1, t.getGenSufragio());
+			stmt.setInt(2, t.getGenId());
+			stmt.registerOutParameter(3, Types.INTEGER);
+			stmt.execute();
+			if (stmt.getInt(3) >= 1) {
+				return Boolean.TRUE;
+			}
+		} catch (Exception e) {
+			logger.error("Error" + e);
+		}finally {
+			stmt.close();			
+		}
+		return Boolean.FALSE;
 	}
 
 	@Override
@@ -64,8 +114,25 @@ public class SufragioImpl extends Conexion implements Service<Sufragio>, Seriali
 
 	@Override
 	public Sufragio finById(Integer id) throws SQLException {
-		
-		return null;
+		CallableStatement stmt = null;
+		Sufragio g =  null;
+		try {
+			String query = "{CALL SP_READ_ONE_GEN(?)}";
+			stmt = getConnection().prepareCall(query);
+			stmt.setInt(1, id);
+			setRs(stmt.executeQuery());
+			if (getRs().next()) {
+				getRs().beforeFirst();
+				while (getRs().next()) {
+					g = new Sufragio(getRs().getInt(1), getRs().getString(2));
+				}
+			}
+		} catch (Exception e) {
+			logger.error("Error" + e);
+		}finally {
+			stmt.close();
+		}
+		return g;
 	}
 
 	@Override
